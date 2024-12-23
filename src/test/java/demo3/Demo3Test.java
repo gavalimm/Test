ada51c21c304a0fc130638738e45cc8626d207a8
manojmgavali <gavalimanojm7@gmail.com>
2024-12-23 22:33:24 +0530

    "commited multiple packages"

---
+++ src/test/java/demo3/Demo3Test.java
@@ -0,0 +1,9 @@
+package demo3;
+
+public class Demo3Test {
+
+	public static void main(String [] args)
+	{
+		System.out.println("this is demo3 package");
+	}
+}
---
+++ src/test/java/demo4/Demo4Test.java
@@ -0,0 +1,9 @@
+package demo4;
+
+public class Demo4Test {
+	public static void main(String [] args)
+	{
+		System.out.println("this is demo4 package");
+	}
+
+}
